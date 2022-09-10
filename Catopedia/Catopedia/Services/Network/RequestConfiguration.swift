43e950c370144981374d5877bc0ffc48a3362ec4
Tania Jasam <tania.j@pepperfry.com>
2022-09-10 15:31:06 +0530

    Network Manager created

---
+++ Catopedia/Catopedia/Services/Network/RequestConfiguration.swift
@@ -0,0 +1,38 @@
+//
+//  RequestConfiguration.swift
+//  Catopedia
+//
+//  Created by Tania Jasam on 10/09/22.
+//
+
+import Foundation
+
+enum HTTPRequestMethod: String {
+    case GET, POST, PUT, DELETE
+}
+
+protocol RequestConfiguration {
+    
+    func getURL() -> URL?
+    
+    func getMethod() -> HTTPRequestMethod
+    
+    func getHeaders() -> [String: String]
+    
+    func getRequestBody() -> [String: Any]?
+}
+
+extension RequestConfiguration {
+    
+    func getMethod() -> HTTPRequestMethod {
+         .GET
+    }
+    
+    func getHeaders() -> [String: String] {
+         [:]
+    }
+    
+    func getRequestBody() -> [String: Any]? {
+         nil
+    }
+}
